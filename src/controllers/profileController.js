12987ba266929cb5f93d8ce669ff26de96e8d3b0
TDQuecHi <huyt02731@gmail.com>
2026-05-08 19:11:25 +0700

    chức năng đăng nhập

--- src/controllers/profileController.js
+++ src/controllers/profileController.js
@@ -1,8 +1,14 @@
 const userProfile = (req, res) => {
-  return res.status(200).json({ message: "This is user profile" });
+  return res.status(200).json({
+    message: "This is user profile",
+    data: { userId: req.user.id, role: req.user.role },
+  });
 };
 const adminProfile = (req, res) => {
-  res.send("This is admin profile");
+  return res.status(200).json({
+    message: "This is admin profile",
+    data: { userId: req.user.id, role: req.user.role },
+  });
 };
 
 module.exports = {
